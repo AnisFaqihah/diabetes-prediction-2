659aa14a42e8d94e11f5cfce4d91a4b59e608817
AnisFaqihah <81802776+AnisFaqihah@users.noreply.github.com>
2022-01-03 17:09:40 +0800

    Add files via upload

---
+++ diabetes.py
@@ -0,0 +1,100 @@
+#import libraries
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from PIL import Image
+import streamlit as st
+
+#Create title and sub-title
+st.write("""
+# Diabetes Prediction
+Predict if someone has diabetes using Random Forest!
+""")
+
+#Open and display image
+image1 = Image.open('diabetes1.png')
+st.image(image1, use_column_width=True)
+
+#Get the data
+path = "https://github.com/AnisFaqihah/Diabetes-Prediction/raw/main/diabetes.csv"
+df = pd.read_csv(path)
+
+#Set a subheader
+st.subheader('Data Information')
+
+#Show the data as table
+st.dataframe(df)
+
+st.markdown("***")
+
+#Show statistics on the data
+st.write(df.describe())
+
+st.markdown("***") 
+
+#Show the data as a chart
+chart = st.bar_chart(df)
+
+#Split the data into independent 'X' and dependent 'Y' variables
+X = df.iloc[:, 0:8].values
+Y = df.iloc[:, -1].values
+
+#Split the dataset into 75% Training and 25% Testing
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.25,random_state=0)
+
+#Get the feature input from user
+def get_user_input():
+    pregnancies = st.sidebar.slider('pregnancies', 0, 17, 3)
+    glucose = st.sidebar.slider('glucose', 0, 199, 117)
+    blood_pressure = st.sidebar.slider('blood_pressure', 0, 122, 72)
+    skin_thickness = st.sidebar.slider('skin_thickness', 0, 99, 23)
+    insulin = st.sidebar.slider('insulin', 0.0, 846.0, 30.5)
+    BMI = st.sidebar.slider('BMI', 0.0, 67.1, 32.0)
+    DPF = st.sidebar.slider('DPF', 0.078, 2.42, 0.3725)
+    age = st.sidebar.slider('age', 21, 81, 29)
+
+    #Store dictionary into variable
+    user_data = {'pregnancies' : pregnancies,
+                 'glucose' : glucose,
+                 'blood_pressure' : blood_pressure,
+                 'skin_thickness' : skin_thickness,
+                 'insulin' : insulin,
+                 'BMI' : BMI,
+                 'DPF' : DPF,
+                 'age' : age
+                }
+    #Transform the data into a data frame
+    features = pd.DataFrame(user_data, index = [0])
+    return features
+
+#Store the user input into a variable
+user_input = get_user_input()
+
+#Open and display image
+image2 = Image.open('diabetes2.png')
+st.image(image2, use_column_width=True)
+
+#Set a subheader and display the users input
+st.subheader('User Input: ')
+st.write(user_input)
+
+#Create and train the model
+RandomForestClassifier = RandomForestClassifier()
+RandomForestClassifier.fit(X_train, Y_train)
+
+#Show the model metrics
+st.subheader('Model Test Accuracy Score:')
+st.write(str(accuracy_score(Y_test,RandomForestClassifier.predict(X_test)) * 100)+'%')
+
+#Store the models predictions in variable
+prediction = RandomForestClassifier.predict(user_input)
+
+#Set a subheader and display the prediction
+st.subheader('Prediction: ')
+st.write(prediction)
+
+#Open and display image
+image3 = Image.open('diabetes3.png')
+st.image(image3, use_column_width=True)
