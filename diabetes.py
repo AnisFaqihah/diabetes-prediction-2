6c96721b858b1f500cf20e0d26832211ee686c92
AnisFaqihah <81802776+AnisFaqihah@users.noreply.github.com>
2022-01-26 22:04:08 +0800

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -94,9 +94,9 @@ tree_cv = RandomizedSearchCV(RandomForestClassifier, param_dist, cv = 5)
 tree_cv.fit(X_train, Y_train)
   
 # Print the tuned parameters and score
-st.subheader('Tuned Decision Tree Parameters: ')
+st.subheader('Tuned Decision Tree Parameters:')
 st.write(tree_cv.best_params_)
-st.subheader('Best score: ')
+st.subheader('Best score:')
 st.write(tree_cv.best_score_)
 
 #Newline
